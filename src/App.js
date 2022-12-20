00e61241fd41fa834dc364ef2ea0e28ee13a8453
kailash-unc <kailash@email.unc.edu>
2022-12-20 15:43:12 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SortVisualizer from './SortVisualizer/SortVisualizer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SortVisualizer></SortVisualizer>
+    </div>
+  );
+}
+
+export default App;
